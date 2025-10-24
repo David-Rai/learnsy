95ea8e0d89c6e030bf50b878ac270d160aa07968
david-rai <davidkhaling123@gmail.com>
2025-10-24 11:57:34 +0545

    Feat:Checking if all answers are answered on current selected category lesson

--- src/utils/checkIsAnsweredAll.jsx
+++ src/utils/checkIsAnsweredAll.jsx
@@ -1,13 +1,23 @@
-import supabase from "../config/supabase"
-import useHomeStore from "../context/store"
+import supabase from "../config/supabase";
+import useHomeStore from "../context/store";
 
-const checkIsAnsweredAll=()=>{
-const {answers,currentLesson,currentCategory}=useHomeStore.getState()
+const checkIsAnsweredAll = () => {
+  const {
+    answers,
+    currentLesson,
+    currentCategory,
+    lessons = [],
+  } = useHomeStore.getState();
+  const currentSelectedLesson = lessons.find(
+    (l) => l.name === currentLesson.name
+  );
 
-console.log("checking if all answers are answered")
-console.log(currentLesson)
+  if(currentSelectedLesson?.questions && currentSelectedLesson?.maxReached === true){
+  console.log("checking if all answers are answered");
+console.log(currentSelectedLesson)
 console.log(answers)
+  }
 
-}
+};
 
-export default checkIsAnsweredAll
+export default checkIsAnsweredAll;
