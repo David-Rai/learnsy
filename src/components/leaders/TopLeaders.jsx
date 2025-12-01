f79902b4786dee6e2df5f192d36746ac7aef352c
david-rai <davidkhaling123@gmail.com>
2025-12-02 00:11:13 +0545

    Fix(Profile):Finally you can see the top player profile

--- src/components/leaders/TopLeaders.jsx
+++ src/components/leaders/TopLeaders.jsx
@@ -1,12 +1,17 @@
 import React, { memo } from 'react'
+import { useNavigate } from 'react-router'
 
 const TopLeader = memo(({ l, rank }) => {
-  const { username, avatar, points, total_questions, wrong_questions } = l
+  const { username, avatar, points, total_questions, wrong_questions,user_id } = l
   const right_questions = total_questions - wrong_questions
+  const navigate=useNavigate()
   const accuracy = total_questions === 0 ? 0 : ((right_questions / total_questions) * 100).toFixed(1)
 
   return (
-    <section className="flex flex-col items-center justify-center bg-[var(--bg)] text-[var(--text)] p-6 rounded-2xl shadow-lg w-full max-w-sm mx-auto">
+    
+    <section
+      onClick={() => navigate(`/profile/${user_id}`)}
+    className="flex flex-col items-center justify-center cursor-pointer bg-[var(--bg)] text-[var(--text)] p-6 rounded-2xl shadow-lg w-full max-w-sm mx-auto">
       <div className="relative flex flex-col items-center">
         <img
           src={avatar}
